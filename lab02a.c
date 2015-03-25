d79523f5213cd57daaa25d2c5488c62b5adabe09
André Almeida <fda.andre@gmail.com>
2015-03-25 23:25:37 +0000

    vish

--- lab02a.c
+++ lab02a.c
@@ -3,14 +3,31 @@
 
 int main(){
   
-  char x;
-  int y;
+  char x, aux;
+  int y, divisor = 0, dividendo = 0;
+  float resultado;
   
-   while(x!='*'){
-      scanf("%c %d", &x, &y);
-      printf("%d\n", (69-x));
+  while(x!='*'){
+    scanf("%c", &x);
+    if(x!='*'){
+      scanf(" %d", &y);
+      scanf("%c", &aux);
+      dividendo += (69-x) * y;
+      divisor += y;
+      /*printf("x=%d\n", x);
+      printf("y=%d\n", y);
+      printf("dividendo=%d\n", dividendo);
+      printf("divisor=%d\n", divisor); */
+    }
   }
- 
-  return 0;
   
+  resultado = (float) dividendo / divisor;
+  /*printf("#dividendo=%d\n", dividendo);*/
+  printf("#divisor=%d\n", divisor);
+  if  (divisor != 0)
+    printf("CR = %.4f\n", resultado);
+  else
+    printf("CR = Indefinido\n");
+  
+  return 0;  
 }
