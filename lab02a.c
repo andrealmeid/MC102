900bf552772c9afd792834f359dd3ad0346999a1
André Almeida <fda.andre@gmail.com>
2015-03-25 18:30:45 +0000

    gogo

---
+++ lab02a.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+  
+  char x;
+  int y;
+  
+   while(x!='*'){
+      scanf("%c %d", &x, &y);
+      printf("%d\n", (69-x));
+  }
+ 
+  return 0;
+  
+}
