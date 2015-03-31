e8daf0ffe326b0ae36779ae575fc7d96f0181d80
André Almeida <fda.andre@gmail.com>
2015-03-31 16:19:21 +0000

    3a foi

--- lab03a.c
+++ lab03a.c
@@ -7,26 +7,36 @@
 #define MX 100
 
 int main(){
-    
-    int l, c, i, tot[MX];
-    char get;
-    
-    for(i=0;i<MX;i++)
-        tot[i]=0;
-    
-    
-    scanf("%d %d", &l, &c);
-    
-    for(i=0;i<=l;i++){
-        do{
-        scanf("%c", &get);
-	tot[(int)get-48]+=1;
-        }while(get!=10);
-    }
-    
-    for(i=1;i<=c;i++)
-    printf("tot[%d]: %d\n", i, tot[i]);
-    
-    
-    return 0;
+  
+  int l, c, i, tot[MX], pos, total=0;
+  char get;
+  double porc;
+  
+  for(i=0;i<MX;i++)
+    tot[i]=0;
+  
+  
+  scanf("%d %d", &l, &c);
+  
+  for(i=0;i<=l;i++){
+    do{
+      scanf("%c", &get);
+      pos = (int)get-48;
+      tot[pos]+=1;
+      if(get!=10 && get!=32)
+	total+=1;
+    }while(get!=10);
+  }
+  
+  printf("Total: %d individuos\n", total);
+  
+  for(i=1;i<=c;i++)
+    printf("Total da casta %d: %d\n", i, tot[i]);
+  
+  for(i=1;i<=c;i++){
+    porc = ((tot[i]*100.0)/total);
+    printf("Percentual da casta %d: %.2f\n", i, porc);
+  }
+  
+  return 0;
 }
