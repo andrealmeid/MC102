d89c75344568a2d2107a60189af238729ede151d
André Almeida <fda.andre@gmail.com>
2015-03-31 19:32:20 +0000

    pronto

--- lab03b.c
+++ lab03b.c
@@ -8,7 +8,7 @@
 
 int main(){
   
-  /*variaveis*/
+  /* variaveis*/
   int n, g[MX], i, j, min[MX], max[MX], nSinc=0;
   
   for(i=0;i<MX;i++){
@@ -17,23 +17,28 @@ int main(){
     max[i]=0;
   }
   
-  /*entrada*/
+  /* entrada*/
   scanf("%d", &n);
   
+  /* recebe os intervalos e guarda o num de vezes que cada total de sinceros aparece*/
   for(i=1;i<=n;i++){
     scanf("%d %d", &min[i], &max[i]);
     for(j=min[i];j<=max[i];j++)
       g[j]++;
   }
   
+  /* verifica qual o numero com mais ocorrencia
+  *  esse numero sera o total de guardioes sinceros
+  *  se ele for coerente com o numero de ocorrencias*/
   for(i=1;i<=n;i++){
     if(g[i]>=g[nSinc] && i==g[i])
       nSinc=i;
   }
   
+  /* saida*/
   printf("Numero de guardioes sinceros = %d\n", nSinc);
   
-  /*procura o primeiro guardiao que cita o numero correto*/
+  /* procura e imprime o primeiro guardiao que cita o numero correto*/
   for(i=1;i<=n;i++){
     for(j=min[i];j<=max[i];j++)
       if(j==nSinc){
