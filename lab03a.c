f4a8e624ff8168f1853f36de0821d4aa9e298b85
André Almeida <fda.andre@gmail.com>
2015-03-31 03:27:41 +0000

    hard

---
+++ lab03a.c
@@ -0,0 +1,26 @@
+/* Nome: André Figueiredo de Almeida
+ * RA: 164047
+ * Laboratorio 03a - Admiravel Mundo Novo */
+
+#include <stdio.h>
+#include <math.h>
+#define MX 100
+
+int main(){
+    
+    int l, c, i;
+    char get;
+    
+    /*for(i=0;i<MX;i++)
+        tot[i]=0;*/
+    
+    
+    scanf("%d %d", &l, &c);
+    
+    for(i=0;i<l;i++){
+        while(get!=10){
+        scanf("%c", &get);
+        }
+    }
+    return 0;
+}
