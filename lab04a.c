ccbc51307da5a8abc334ca12753ba75ff00e6b1d
André Almeida <fda.andre@gmail.com>
2015-04-07 15:02:03 +0000

    0.2

--- lab04a.c
+++ lab04a.c
@@ -7,32 +7,44 @@
 #define M 11
 
 int main(){
-        
-    int x[M], y, i;
-    char mov;
-    
-    for(i=0;i<M;i++)
-        x[i]=0;
-
-    scanf("%d %d", &x[0], &y);
-    
+  
+  int x[M], y, i, pos = 1, box = 0;
+  char mov;
+  
+  for(i=0;i<M;i++)
+    x[i]=0;
+  
+  /*a posicao 0 do vetor guarda o numero de posicoes*/
+  scanf("%d %d", &x[0], &y);
+  
+  /*cada i>0 do vetor guarda seu respectivo numero de caixas*/
+  for(i=1;i<=x[0];i++)
+    scanf("%d", &x[i]);
+  
+  do{
+    scanf("%c", &mov);
+    switch(mov){
+      case 'D':
+	if(box==0){
+	  x[pos]-=1;
+	  box=1;
+	}else{
+	  x[pos]+=1;
+	  box=0;
+	}
+	break;
+      case 'R':
+	pos+=1;
+	break;
+      case 'L':
+	pos-=1;
+	break;
+    }
+  }while(mov!='*');
+  
     for(i=1;i<=x[0];i++)
-        scanf("%d", &x[i]);
-    
-    do{
-        scanf("%c", &mov);
-        switch(mov){
-            case 'D':
-                printf("down\n");
-                break;
-            case 'R':
-                printf("right\n");
-                break;
-            case 'L':
-                printf("left\n");
-                break;
-        }
-    }while(mov!='*');
-    
-    return 0;
+    printf("%d ", x[i]);
+  
+    printf("\n");
+  return 0;
 }
