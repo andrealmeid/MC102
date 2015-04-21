bbe5501abb98d67ea358566bb5c77eab0f0e2629
André Almeida <fda.andre@gmail.com>
2015-04-21 04:06:28 +0000

    0.1

---
+++ lab06a.c
@@ -0,0 +1,27 @@
+/* Nome: André Figueiredo de Almeida
+ * RA: 164047
+ * Laboratorio 06a - Raio Sugestor */
+
+#include <stdio.h>
+#include <string.h>
+#define TAM 50
+
+int main(){
+    
+    int cidade[50][50], m, n, i, j, r;
+    
+    scanf("%d %d %d", &m, &n, &r);
+    
+    for(i=0;i<m;i++)
+        for(j=0;j<n;j++)
+            scanf("%d", &cidade[i][j]);
+    
+    /* transforma o alcance nas dimensoes da matriz 'area afetada' 
+     * como a matriz é quadrada, as duas dimensoes tem o mesmo tamanho */
+    r = (2 * r) + 1;
+    
+    for
+    
+    
+        return 0;
+}
