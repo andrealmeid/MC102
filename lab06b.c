25ba80526ebb9b38164ef718fe8e57b107396919
André Almeida <fda.andre@gmail.com>
2015-04-27 02:53:29 +0000

    almost 06b

--- lab06b.c
+++ lab06b.c
@@ -8,7 +8,10 @@
 int contaminar(int *p1, int *p2, int *p3, int *p4, int *limp){
   /* como um espaco contamidado pode contaminar os outros, ele pode ser
    * considerado um dispersor; nesse metodo, as posicoes sao marcadas com 
-   * o valor 3, que signica pre-contamidas para depois virarem dispersores*/
+   * o valor 3, que signica pre-contamidas para depois virarem dispersores
+   * p1-p4 sao ponteiros que apontam para as coordenadas dos espacos 
+   * o retorno informa se ocorreu alguma contaminacao bem sucedida*/
+  
   int retorno=0;
   
   if(*p1==0){
@@ -43,7 +46,7 @@ int main(){
   /* rcnt guarda as caracteristicas do recinto;
    * tempo é o tempo de dispersao;
    * limpos guarda os espacos vazios
-   * agiu verifica se alguma contaminacao ocorreu*/
+   * agiu verifica se alguma contaminacao ocorreu */
   int rcnt[TAM][TAM], m, n, tempo=0, i, j, limpos=0, agiu=-1;
   
   /* inicializa a matriz preenchendo ela com paredes */
@@ -67,7 +70,9 @@ int main(){
       if(rcnt[i][j]==0)
 	limpos++;
 	
-  /* verificacao e acao dos aspersores */
+  /* verificacao e acao dos aspersores 
+   * o laco so continua se agiu for negativo, ou seja, se ocorrer uma 
+   * contaminacao no laco anterior */
   while (agiu<0){
     agiu=0;
     for(i=1;i<=m;i++)
@@ -76,15 +81,17 @@ int main(){
 	  rcnt[i][j]--;
     for(i=1;i<=m;i++)
       for(j=1;j<=n;j++)
-	if(rcnt[i][j]==2){
-	  agiu -= contaminar(&rcnt[i+1][j],&rcnt[i-1][j],
-			    &rcnt[i][j-1],&rcnt[i][j+1], &limpos);
+	    if(rcnt[i][j]==2){
+	    /* se o lugar for um dispersor, ele ira passar as coordenadas dos
+	     * espacos vizinhos para o metodo contaminar */
+	        agiu -= contaminar(&rcnt[i+1][j],&rcnt[i-1][j],
+			                   &rcnt[i][j-1],&rcnt[i][j+1], &limpos);
 	}
     tempo++;
   }
 	
   /* se todos os espacos vazios estiverem contaminados,
-     imprimir o tempo de contaminacao */
+   * imprimir o tempo de contaminacao */
     if(limpos==0)
       printf("Tempo = %d\n", tempo);
     else
