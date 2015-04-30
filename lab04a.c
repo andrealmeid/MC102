093347346ac0b592818ee57b975d23614ab21ea6
André Almeida <fda.andre@gmail.com>
2015-04-30 20:14:34 +0000

    04a fisinhed

--- lab04a.c
+++ lab04a.c
@@ -1,114 +1,74 @@
 /* Nome: André Figueiredo de Almeida
  * RA: 164047
- * Laboratorio 07a - Cargo-Bot - Part II*/
+ * Laboratorio 04a - Cargo-Bot - Part I*/
 
 #include <stdio.h>
-#include <string.h>
-
-#define TRUE 1
-#define FALSE 0
+#include <math.h>
+#define M 11
 
 int main(){
-   
-   /*VARIAVEIS*/
-   /* x=posicoes para empilhamento de caixas; y=numero max de empilhamento */
-   int x, y;
-   
-   /* p = num de programas; c = num max de comandos de programas*/
-   int p, c;
-   
-   /* e = num de execucoes de programas; i, j, k, l = contadores */
-   int e, i, j, k, l, num;
-   
-   /* boxes = posicionamento das caixas */
-   char boxes[10][10];
-
-   /* sqnc = sequencia dos programas*/
-   char sqnc[60];
-  
-   /* progs = armazena cada programa em uma linha, duas pos extras foram
-    * adicionada para guardar o asterisco e o \0 */
-   char progs[9][62];
-  
-   /* garra = posicao da garra; acidente indica se ocorreu algum acidente */
-   int garra=0, acidente = FALSE;
-   
-   /* hold = guarda o que a garra segura
-    * . = nada, B = caixa azul, G = caixa verde*/
-   char hold='.';
-   
-   /*ENTRADAs*/
-   /* entrada da primeira linha */
-   scanf("%d %d %d %d %d", &x, &y, &p, &c, &e);
-  
-   /* entrada das posicoes iniciais das caixas */
-   for(i=0;i<y;i++)
-      scanf("%s", boxes[i]);
     
-   /* entrada da sequencia de execucao dos programas */
-   scanf(" ");
-   for(i=0;i<e;i++)
-      scanf("%c", &sqnc[i]);
-  
-   /* entrada dos comandos dos programas */
-   for(i=0;i<p;i++)
-      scanf("%s", progs[i]);
-   
-   /*PROCESSAMENTOS*/
-
-   for(k=0;k<e;k++){
-      i=0;
-      num=sqnc[k]-'1';
-      printf("%d\n", num);
-      while(progs[num][i]!='*'){
-         j=0;
-         switch(progs[0][i]){
+    /*box = 0 sem caixa; box = 1 com  caixa*/
+    /*y guarda a altura maxima; pos guarda a posicao*/
+    int x[M], y, i, pos = 1, box = 0, acidente=0;
+    char mov;
+    
+    for(i=0;i<M;i++)
+        x[i]=0;
+    
+    /*a posicao 0 do vetor guarda o numero de posicoes de empilhamento*/
+    scanf("%d %d", &x[0], &y);
+    
+    /*cada x[i>0] do vetor guarda seu respectivo numero de caixas*/
+    for(i=1;i<=x[0];i++)
+        scanf("%d", &x[i]);
+    
+    do{
+        scanf("%c", &mov);
+        switch(mov){
+            /*verifica se existe caixa a ser pega*/
             case 'D':
-               if(hold=='.'){
-                  while(boxes[j][garra]=='.' && j<y-1)
-                     j++;
-                  hold=boxes[j][garra];
-                  boxes[j][garra]='.';
-               } else {
-                  for(j=0;j<y;j++)
-                     if(boxes[j+1][garra]!='.')
-                        break;
-                     boxes[j][garra]=hold;
-                  hold='.';
-               }
-               break;
-               
+                if(box==0){
+                    if(x[pos]>0){
+                        x[pos]-=1;
+                        box=1;
+                    }
+                }else{
+                    x[pos]+=1;
+                    box=0;
+                }
+                break;
+            /*verifica se existe a prox posicao e se nao ocorreu acidente*/
             case 'R':
-               if(garra<x)
-                  garra++;
-               break;
-               
+                if(pos<x[0]){
+                    if(x[pos+1]==y && box==1){
+                        acidente=pos+1;
+                    } else
+                        pos+=1;
+                }
+                break;
+            /*a mesma coisa do caso R, so que para esquerda*/
             case 'L':
-               if(garra>0)
-                  garra--;
-               break;
-               
-            case 'B':
-               if(hold!='B')
-                  i++;
-               break;
-               
-            case 'G':
-               if(hold!='G')
-                  i++;
-               break;
-         }
-         /*printf("comando=%c j=%d garra=%d hold=%c",progs[0][i], j, garra, hold);
-          *   printf("\n");          */
-         
-         
-         i++;
-      }
-   }
-   
-   for(i=0;i<y;i++){
-      printf("%s", boxes[i]);
-      printf("\n");    
-      }
-   return 0;
+                if(pos>1){
+                    if(x[pos-1]==y && box==1){
+                        acidente=pos+1;
+                    } else
+                        pos-=1;
+                    break;
+                }
+        }
+    }while(mov!='*');
+    
+    
+    /*imprime o resultado*/
+    if(acidente)
+        printf("Um acidente ocorreu");
+    else{
+        for(i=1;i<x[0];i++)
+            printf("%d ", x[i]);
+        printf("%d", x[i]);
+    }
+    
+    printf("\n");
+    return 0;
 }
