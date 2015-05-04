9bc409509cf35efd1294ea772594e7c4665344e9
André Almeida <fda.andre@gmail.com>
2015-05-04 12:43:05 +0000

    07b fim

--- lab07b.c
+++ lab07b.c
@@ -4,29 +4,36 @@
 
 #include <stdio.h>
 
-#define TAM 51
+#define TAM 50
 #define FALSE 0
 #define TRUE 1
 
 int main(){
    
-   /* mapa armazena as casas do tabuleiro, n = linhas, m = colunas */
+   /* mapa armazena as casas do tabuleiro; mapaLoop verifica se o robo
+    * esta repetindo as posicoes; peca = casa do tabuleiro atual */
    char mapa[TAM][TAM], mapaLoop[TAM][TAM], peca;
-   int n, m, fim, loop = 0;
    
-   /* pos = posicao atual; mov = numero de movimentos */
-   int posX, posY, mov = 0, i, j, k, l;
+   /* n = linhas, m = colunas; fim = aponta se o caminho acabou ou
+    * se deve ser interrompido; loop = conta o tamanho do loop */
+   int n, m, fim, loop;
    
+   /* pos = posicao atual do robo; mov = numero de movimentos */
+   int posX, posY, mov, i, j, k, l;
+   
+   /* inicializa a matriz mapaLoop */
    for(i=0;i<TAM;i++)
       for(j=0;j<TAM;j++)
          mapaLoop[i][j]=0;
    
+      
    scanf("%d %d", &n, &m);
    
    for(i=0;i<n;i++)
       scanf("%s", mapa[i]);
    
    
+   /* percorre todos os n caminhos */
    for(i=0;i<n;i++){
       /* reinicializacao das variaveis */
       fim=FALSE;
@@ -38,40 +45,47 @@ int main(){
       for(k=0;k<TAM;k++)
          for(l=0;l<TAM;l++)
             mapaLoop[k][l]=0;
-      
+     
       while(fim==FALSE){      
          switch(peca){
             case 'R':
-               posX++;
+               /* verifica se a proxima posicao nao esta fora dos limites 
+                * da matriz. se sim, imprime o resultado e encerra o caminho 
+                * se nao, move uma casa pra direita */
+               if(posX+1>m-1){
+                  printf("Saiu em (%d,%d) apos %d passo(s)\n", 
+                        posY+1, posX+1, mov);
+                  fim=TRUE;
+               } else
+                  posX++;
                break;
-               
+            
+            /* move uma casa para esquerda */
             case 'L':
                posX--;
                break;
-               
+            
+            /* move uma casa para cima */
             case 'U':
                posY--;
                break;
-               
+
+            /* move uma casa para baixo */               
             case 'D':
                posY++;
                break;
-               
+            
+            /* se a casa for uma armadilha, imprime a posicao e finaliza */
             case 'X':
                printf("Armadilha em (%d,%d) apos %d passo(s)\n",
-                      posY+1, posX+1, mov);
+                        posY+1, posX+1, mov);
                fim=TRUE;
-               break;
-               
+               break;     
          }
          
-         
-         if(posX>m-1){
-            printf("Saiu em (%d,%d) apos %d passo(s)\n", 
-                   posY+1, posX, mov);
-            fim=TRUE;
-         }
-         
+         /* bloco que verifica se entrou em loop infinito.
+          * se o robo passar uma segunda vez no mesmo lugar, um contador 
+          * aumenta ate que ele passe uma terceira vez e interrompe a linha*/
          mapaLoop[posY][posX]++;
          if(mapaLoop[posY][posX]>=2){
             if(mapaLoop[posY][posX]==3){
@@ -79,12 +93,14 @@ int main(){
                fim=TRUE;
             } else
                loop++;
-         }
+         } 
          
+         /* aumenta a contagem dos movimentos a cada ciclo */
          mov++;
+         /* atribui a peca o valor da casa do prox ciclo */
          peca = mapa[posY][posX];
       }
    }
- 
+   
    return 0;
 }
