2ab91a09a68a58fbe3431668bdbd999b33c764c0
André Almeida <fda.andre@gmail.com>
2015-05-17 07:48:53 +0000

    09a almost

--- lab09a.c
+++ lab09a.c
@@ -1,15 +1,22 @@
-/* Nome: André Figueiredo de Almeida
+/* Nome: Andre Figueiredo de Almeida
  * RA: 164047
  * Laboratorio 09a - A Batalha de Geonosis */
 
 #include <stdio.h>
 #include <stdlib.h>
 
+int disparos(int a){
+    if(a==0)
+        return 0;
+    else
+        return 1;
+}
+
 int main(){
     
-    int m, n, **campo, i, j, k, l, aux, total, maior=0, Xmaior=0, Ymaior=0;
+    int m, n, **campo, i, j, k, l, aux, total, maior=0, xMaior=0, yMaior=0;
     
-    int *linhas, *colunas, dp=0, ds=0;
+    int *linhas, *colunas, dp=0, ds=0, dMaior=5, disp=0;
     
     
     scanf("%d %d", &m, &n);
@@ -28,95 +35,103 @@ int main(){
     for(i=0;i<m;i++)
         campo[i] = malloc(n * sizeof(int));
     
-    for(i=0;i<m;i++)
-        for(j=0;j<n;j++)
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
             scanf("%d", &campo[i][j]);
-        
-        for(i=0;i<m;i++){
-            for(j=0;j<n;j++){
-                if(campo[i][j]==1)
-                    linhas[i]++;
-            }
         }
-        
-        for(i=0;i<n;i++){
-            for(j=0;j<m;j++){
-                if(campo[j][i]==1)
-                    colunas[i]++;
-            }
+    }
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            if(campo[i][j]==1)
+                linhas[i]++;
         }
-        
-        i=0, j=0, k=0, l=0;
-        for(i=0;i<m;i++){
-            for(j=0;j<n;j++){
-                if(campo[i][j]!=1){
-                printf("ponto[%d][%d]:%d\n", i, j, campo[i][j]);
-                k=i, l=j;
+    }
+    
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            if(campo[j][i]==1)
+                colunas[i]++;
+        }
+    }
+    
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            if(campo[i][j]!=1){
                 dp=0, ds=0;
                 
-                while(k<m && l<n){
-                    dp+=campo[k][l];
-                    printf("campo1[%d][%d]:%d\n", k, l, campo[k][l]);
+                k=i, l=j;
+                while(k<m-1 && l<n-1){
                     k++;
                     l++;
+                    dp+=campo[k][l];
                 }
                 
                 k=i, l=j;
-                
                 while(k>0 && l>0){
                     k--;
                     l--;
-                    printf("campo2[%d][%d]:%d\n", k, l, campo[k][l]);
                     dp+=campo[k][l];
-                }
-
-                 k=i, l=j;
-                while(k<m && l>0){
-                    ds+=campo[k][l];
-                    printf("campo3[%d][%d]:%d\n", k, l, campo[k][l]);
-                    k++;
-                    l--;
                 }
                 
                 k=i, l=j;
+                while(k<m-1 && l>0){
+                    k++;
+                    l--;
+                    ds+=campo[k][l];             
+                }
                 
-                while(k>0 && l<n){
+                k=i, l=j;                
+                while(k>0 && l<n-1){
                     k--;
                     l++;
-                    printf("campo4[%d][%d]:%d\n", k, l, campo[k][l]);
                     ds+=campo[k][l];
                 }
                 
                 
+                total = dp + ds + linhas[i] + colunas[j];  
+                disp = disparos(dp) + disparos(ds) + disparos(linhas[i]) +
+                disparos(colunas[j]);
                 
-                printf("dp: %d ds: %d\n", dp, ds);
-                total = dp + ds + linhas[i] + colunas[j];
-                printf("total:%d\n", total);
                 if(total>maior){
                     maior=total;
-                    Xmaior=i;
-                    Ymaior=j;
+                    dMaior=disp;
+                    xMaior=i;
+                    yMaior=j;
                 }
+                
+                if(total==maior && disp<dMaior){
+                    dMaior=disp;
+                    xMaior=i;
+                    yMaior=j;
+                }
+
+                if(total==maior && disp==dMaior){
+                    if(xMaior+yMaior<i+j)
+                        i=i-0;
                 }
             }
         }
-        
-        printf("maior(%d,%d): %d\n", Xmaior, Ymaior, maior);
-        
-        /*for(i=0;i<m;i++)
-         *     printf("linha[%d]=%d\n", i+1, linhas[i]);
-         *  printf("\n");
-         *  
-         *  for(i=0;i<n;i++)
-         *     printf("%d ", colunas[i]);
-         *  printf("\n");*/
-        
-        /*for(i=0;i<m;i++){
-         *     for(j=0;j<n;j++){
-         *        printf("%d ", campo[i][j]);
-         }
-         printf("\n");
-        }*/
-        
-        return 0;
+    }
+    
+    
+    printf("Posicao = (%d,%d)\n", xMaior, yMaior);
+    printf("Droids = %d\n", maior);
+    printf("Disparos = %d\n", dMaior);
+    
+    /*for(i=0;i<m;i++)
+     *     printf("linha[%d]=%d\n", i+1, linhas[i]);
+     *  printf("\n");
+     *  
+     *  for(i=0;i<n;i++)
+     *     printf("%d ", colunas[i]);
+     *  printf("\n");*/
+    
+    /*for(i=0;i<m;i++){
+     *     for(j=0;j<n;j++){
+     *        printf("%d ", campo[i][j]);
+}
+printf("\n");
+}*/
+    
+    return 0;
 }
