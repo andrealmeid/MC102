fad200f5532227fa993e3b43bee2d9afe3455d61
André Almeida <fda.andre@gmail.com>
2015-05-17 22:10:48 +0000

    09a falta o resto

--- lab09b.c
+++ lab09b.c
@@ -5,9 +5,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void negativar(float **jogadores, int nJogadores, int a){
+    int i;
+    
+    for(i=0;i<nJogadores;i++){
+        jogadores[i][a]=-1;
+    }
+}
+
 int main(){
     
-    int nJogadores, nTimes, i, j, **time, mMedia, jTimes;
+    int nJogadores, nTimes, i, j, k, **time, maior, jTimes, capitao;
     
     float **jogadores, *media;
     
@@ -15,18 +23,20 @@ int main(){
     
     media = malloc(nJogadores * sizeof(float));
     
+    /* calculo de jogadores por time */
     jTimes = nJogadores/nTimes;
     
-    time = malloc(nTimes * sizeof(int));
-    for(i=0;i<jTimes;i++)
-        time[i] = malloc(jTimes * sizeof(int*));
-        
+    time = malloc(nTimes * sizeof(int*));
+    for(i=0;i<nTimes;i++)
+        time[i] = malloc(jTimes * sizeof(int));
+    
     for(i=0;i<nJogadores;i++)
         media[i]=0;
     
     jogadores = malloc(nJogadores * sizeof(float*));
     for(i=0;i<nJogadores;i++)
         jogadores[i] = malloc(nJogadores * sizeof(float));
+    
     
     for(i=0;i<nJogadores;i++){
         for(j=0;j<nJogadores;j++){
@@ -35,22 +45,62 @@ int main(){
         }
     }
     
+    /* calculo das medias de notas */
     for(i=0;i<nJogadores;i++)
         media[i]/=nJogadores;
     
+    /* atribuicao dos capitaes de cada time com base nas medias*/
     for(i=0;i<nTimes;i++){
-        mMedia=0;
+        maior=0;
         for(j=1;j<nJogadores;j++){
-            if(media[j]>media[mMedia]){
-            mMedia=j;
+            if(media[j]>media[maior]){
+                maior=j;
             }
         }
-        time[i]=mMedia;
-        media[mMedia]=-1;
+        time[i][0]=maior;
+        media[maior]=-1;
+        negativar(jogadores, nJogadores, maior);
     }
     
-    for(i=0;i<nTimes;i++)
-        printf("%d\n", capitao[i]+1);
+    for(i=0;i<nJogadores;i++){
+        for(j=0;j<nJogadores;j++){
+            printf("%.1f ", jogadores[i][j]);
+        }
+        printf("\n");
+    }
+    
+    for(i=1;i<jTimes;i++){
+        for(j=0;j<nTimes;j++){
+            maior=0;
+            capitao=time[j][0];
+            for(k=1;k<nJogadores;k++){
+                if(jogadores[capitao][k]>jogadores[capitao][maior])
+                    maior=k;
+            }
+            time[j][i]=maior;
+            negativar(jogadores, nJogadores, maior);
+            
+            for(i=0;i<nTimes;i++){
+                for(j=0;j<nJogadores;j++){
+                            capitao=time[i][0];
+                printf("%.1f ", jogadores[capitao][j]);
+            }
+            printf("\n");
+            }
+        }
+        printf("\n");
+    }
+    
+    
+    
+    /* impressao dos valores */
+    for(i=0;i<nTimes;i++){
+        printf("Time %d:", i+1);
+        for(j=0;j<jTimes;j++){
+            printf(" %d", time[i][j]+1);
+        }
+        printf("\n");
+    }
     
     return 0;
 }
