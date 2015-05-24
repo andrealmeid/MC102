f4482c0f9066b7ee07983c4e27bf446053fa6e71
André Almeida <fda.andre@gmail.com>
2015-05-24 00:00:45 +0000

    10a ta bonito

--- lab10a.c
+++ lab10a.c
@@ -4,26 +4,70 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-struct palavra{
-	char *nome;
-	int cont;
+#define FALSE 0
+#define TRUE 1
+
+struct Palavra{
+    char *nome;
+    int cont;
+    int minimo;
 };
 
+typedef struct Palavra Palavra;
+
 int main(){
-	/* d = palavras distintas; f = numero minimo de ocorrencias de uma palavra
-	 * l = tamanho minimo de uma palavra; m = tamanho maxio de uma palavra */
-	int  d, f, l, m;
-	char palavra[10];
-	
-	scanf("%d %d %d %d", &d, &f, &l, &m);
-	
-	
-	do{
-	scanf("%s", palavra);
-	}while(strncmp(palavra,"*",1));
-	
-	
-	return 0;
-	
+    /* d = palavras distintas; f = numero minimo de ocorrencias de uma palavra
+     * l = tamanho minimo de uma palavra; m = tamanho maxio de uma palavra */
+    int  d, f, l, m, i, j;
+    
+    /* aux guarda provisoriamente a palavra lida */
+    char *aux;
+    
+    /* add= booleano para apontar se palavra ja foi adicionada */
+    int add=FALSE;
+    Palavra *palavras;
+    
+    scanf("%d %d %d %d", &d, &f, &l, &m);
+    
+    aux =  malloc(m * sizeof(char));
+    palavras = malloc(d * sizeof(Palavra));
+    for(i=0;i<d;i++){
+        palavras[i].nome = malloc(m * sizeof(char));
+        palavras[i].minimo = FALSE;
+    }
+    
+    i=0;
+    
+    scanf("%s", aux);
+    
+    while(strncmp(aux,"*",1)){
+        add=FALSE;
+        /* verifica se a palavra ja foi armazenada */
+        for(j=0;j<i;j++){
+            /* caso positivo, ele aumenta o contador e quebra o loop */
+            if(strcmp(palavras[j].nome,aux)==0){
+                palavras[j].cont++;
+                printf("Contagem da palavra '%s' = %d\n", 
+                       palavras[j].nome, palavras[j].cont);
+                add=TRUE;
+                j=i;
+            }
+        }
+        if(strlen(aux)>=l && add==FALSE){
+            strcpy(palavras[i].nome, aux);
+            palavras[i].cont=1;
+            printf("Palavra '%s' adicionada.\n", palavras[i].nome);
+            i++;
+        }
+        scanf("%s", aux);
+    }
+
+    for(j=0;j<i;j++){
+        printf("%s %d\n", palavras[j].nome, palavras[j].cont);
+    }
+        
+    
+    return 0;
 }
