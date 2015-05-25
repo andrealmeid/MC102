c6e0dbdf7bc7f9f79749665b5732568e62be8ecc
André Almeida <fda.andre@gmail.com>
2015-05-25 15:57:15 +0000

    10a fim

--- lab10a.c
+++ lab10a.c
@@ -9,6 +9,7 @@
 #define FALSE 0
 #define TRUE 1
 
+/* esse registro guarda as palavras do texto e a quantidade de ocorrencias */
 struct Palavra{
     char *nome;
     int cont;
@@ -22,7 +23,7 @@ int main(){
      * n = numero de palavras armazenadas; auxn auxilia na ordenacao numerica */
     int  d, f, l, m, n=0, i, j, auxn;
     
-    /* aux guarda provisoriamente a palavra lida */
+    /* aux guarda provisoriamente a palavra lida e ajuda na ordenacao */
     char *aux;
     
     /* add = booleano para apontar se palavra ja foi adicionada anteriormente 
@@ -39,6 +40,7 @@ int main(){
     
     scanf("%s", aux);
     
+    /* leia ate encontrar um asterisco */
     while(strncmp(aux,"*",1)){
         add=FALSE;
         /* verifica se a palavra ja foi armazenada */
@@ -50,51 +52,64 @@ int main(){
                 i=n;
             }
         }
+        
+        /* se a palavra for nova, cria um campo para ela no registro e inicia
+         * seu contador no 1 */
         if(strlen(aux)>=l && strlen(aux)<=m && add==FALSE){
             strcpy(palavras[n].nome, aux);
             palavras[n].cont=1;
             n++;
         }
+        
         scanf("%s", aux);
     }
     
-    /* ordenacao das palavras. cada operacao e realizada para o nome e para o 
-     * contador */
-      for(i=1;i<n;i++){
-            j=i;
-            while (j>0 && palavras[j-1].cont<palavras[j].cont) {
-                  strcpy(aux, palavras[j].nome);
-                  auxn = palavras[j].cont;
-                  
-                  strcpy(palavras[j].nome, palavras[j-1].nome);
-                  palavras[j].cont = palavras[j-1].cont;
-                  
-                  strcpy(palavras[j-1].nome, aux);
-                  palavras[j-1].cont = auxn;
-                  
-                  j--;
-            }
-      }
-
-    /* reordenacao conforme valor lexicografico */
-    add = TRUE;
-    while(add){
-        add = FALSE;
-        for(i=0;i<n-1;i++){
-            if(strcmp(palavras[i].nome, palavras[i+1].nome)>0 && 
-                palavras[i].cont==palavras[i+1].cont){
-                strcpy(aux, palavras[i].nome); 
-                strcpy(palavras[i].nome, palavras[i+1].nome);
-                strcpy(palavras[i+1].nome, aux);
-                add = TRUE;
-                }
+    /* ordenacao das palavras conforme o numero de ocorrencias. cada operacao de
+     * troca eh realizada para o nome e para o contador */
+    for(i=1;i<n;i++){
+        j=i;
+        while (j>0 && palavras[j-1].cont<palavras[j].cont) {
+            strcpy(aux, palavras[j].nome);
+            auxn = palavras[j].cont;
+            
+            strcpy(palavras[j].nome, palavras[j-1].nome);
+            palavras[j].cont = palavras[j-1].cont;
+            
+            strcpy(palavras[j-1].nome, aux);
+            palavras[j-1].cont = auxn;
+            
+            j--;
         }
     }
     
-    for(i=0;palavras[i].cont>=f;i++){
-            printf("%s %d\n", palavras[i].nome, palavras[i].cont);
+    /* reordenacao conforme valor lexicografico. se strcmp retorna um valor
+     * positivo, a palavra seguinte eh menor que a atual a ordenacao so
+     * acontece quando as duas palavras possuem a mesma contagem de frequencia.
+     * add mostra se houve alguma troca. se nao houver, a ordenacao terminou */
+    add = TRUE;
+    
+    while(add){
+        add = FALSE;
+        
+        for(i=0;i<n-1;i++){
+            if(strcmp(palavras[i].nome, palavras[i+1].nome)>0 && 
+               palavras[i].cont==palavras[i+1].cont)
+            {
+            strcpy(aux, palavras[i].nome); 
+            strcpy(palavras[i].nome, palavras[i+1].nome);
+            strcpy(palavras[i+1].nome, aux);
+            add = TRUE;
+            }
+                
+        }
     }
-
+    
+    /* sao impressos todas as palavras ate que se encontre a primeira que nao
+     * tem ocorrencias suficientes */
+    for(i=0;palavras[i].cont>=f;i++){
+        printf("%s %d\n", palavras[i].nome, palavras[i].cont);
+    }
+    
     free(aux);
     for(i=0;i<d;i++)
         free(palavras[i].nome);
