3217e0eb14596950432e12066645dff5adbe6205
André Almeida <fda.andre@gmail.com>
2015-06-07 16:25:22 +0000

    12a so por deus

--- lab12a.c
+++ lab12a.c
@@ -7,6 +7,9 @@
 
 #define MAX_FILENAME 25
 #define MAX_ENTITY 10
+#define TAG_INICIO "p class=\"ProfileTweet-text js-tweet-text u-dir\""
+#define TRUE 1
+#define FALSE 0
 
 /* Funcao que converte uma entidade no caractere correspondente,
    supondo que o caractere '&' foi previamente lido no arquivo de
@@ -33,30 +36,96 @@ void converte_entidade(FILE *arqin, FILE *arqout) {
         fprintf(arqout, "\"");
 }
 
+/* Le os outros atributos da tag para nao aparecerem na escrita */
+void limpaTag(FILE *arqin){
+    char c = fgetc(arqin);
+    
+    while(c!='>')
+        c = fgetc(arqin);
+}
+
+/* Escreve no arquivo os tweets */
+void escreveTweet(FILE *arqin, FILE *arqout){
+    char c = fgetc(arqin);
+    
+    while(c!='<'){
+        if(c=='&')
+            converte_entidade(arqin, arqout);
+        else 
+            fprintf(arqout, "%c", c);
+            c = fgetc(arqin);
+        }
+    }        
+
+/* Analisa as tags do codigo HTML para verificar se eh um tweet */
+void analisaTag(FILE *arqin, FILE *arqout, int *escrever){
+    char c, tag_atual[48];
+    int i=0;
+           
+    c = fgetc(arqin);
+    while(c!='>' && i<47){
+       tag_atual[i++] = c;
+       c = fgetc(arqin);
+    }
+    
+    tag_atual[i]='\0';   
+    printf("tag: %s\n", tag_atual);
+    
+    /* se a tag tem os atributos de um tweet */
+    if(!strcmp(tag_atual, TAG_INICIO)){        
+        limpaTag(arqin);       
+        printf(" tag é do tipo inicio\n");
+        *escrever = TRUE;
+        escreveTweet(arqin, arqout);
+        return;
+    }         
+    
+    /* se a tag tem o formato de final de tweet */
+    if(!strcmp(tag_atual, "/p") && *escrever==TRUE){
+        printf(" tag é do tipo final\n");
+        fprintf(arqout, "\n");
+        *escrever = FALSE;
+        return;
+    }
+    
+    /* se a tag pode ser ignorada */    
+    if(*escrever == TRUE){
+        printf(" tag não classificada\n");
+        if(c!='>')
+            limpaTag(arqin);
+        escreveTweet(arqin, arqout);
+    }
+    
+}
+
 void extrai_tweets(char nomearqin[], char nomearqout[]) {
     FILE *arqin = fopen(nomearqin, "r");
     FILE *arqout = fopen(nomearqout, "w");
-    char c;
+    /* c = char auxiliar */
+    char c = fgetc(arqin);  
+    int escrever = FALSE;
     
-    c = fgetc(arqin);
-    while(c != EOF){
-        
-        if(c=='&')
-            converte_entidade(arqin, arqout);
-                
+    /* leitura do arquivo de entrada */
+    while(c != EOF){        
+        /* se encontrar alguma tag, chama o analisaTag() */
+        if(c=='<'){
+                analisaTag(arqin, arqout, &escrever);
+                while(escrever == TRUE)
+                    analisaTag(arqin, arqout, &escrever);
+        }        
         c = fgetc(arqin);
     }
+    
+    fclose(arqin);
+    fclose(arqout);
 }
-
 
 int main(int argc, char *argv[]) {  
     char nomearqin[MAX_FILENAME], nomearqout[MAX_FILENAME];
     
-    scanf("%s %s", nomearqin, nomearqout);
-    
-    /* Copia os nomes do arquivo de entrada e de saida nas variaveis *
+    /* Copia os nomes do arquivo de entrada e de saida nas variaveis */
     strcpy(nomearqin, argv[1]);
-    strcpy(nomearqout, argv[2]);*/
+    strcpy(nomearqout, argv[2]);
  
     extrai_tweets(nomearqin, nomearqout);
 
