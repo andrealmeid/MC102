94dd68184778c4d020582af200fdac8cbb062156
André Almeida <fda.andre@gmail.com>
2015-06-22 03:54:08 +0000

    14b fim

--- lab14b.c
+++ lab14b.c
@@ -5,71 +5,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int menor=100000000;
-
-int real(int n, int nC, int combs[], int **infl){
+/* essa funcao percorre as linhas dos parlamentares selecionados da matriz 
+ * influencia retirando influencias repetidas para se obter o numero real de 
+ * parlamentares */
+int retiraRepetidos(int n, int nParls, int *combs, int **infl){
+    /* r = repetidos, aux = matriz auxiliar que recebe infl */
     int i, j, k, r=0, **aux;
     
+    /* alocacao da matriz aux e clonagem da matriz infl */
     aux = malloc(n * sizeof(int *));
     for(i=0;i<n;i++)
         aux[i] = malloc(n * sizeof(int));
     
     for(i=0;i<n;i++)
         for(j=0;j<n;j++)
-            aux[i][j]=infl[i][j];    
+            aux[i][j]=infl[i][j];
     
-    for(i=0;i<nC-1;i++){
-        for(j=i+1;j<nC;j++){
-            for(k=0;k<n;k++){
+    /* se duas linhas influenciam a mesma coluna, r eh incrementado e a
+     * posicao zerada */
+    for(i=0;i<nParls-1;i++)
+        for(j=i+1;j<nParls;j++)
+            for(k=0;k<n;k++)
                 if(aux[combs[i]][k]==1 && aux[combs[j]][k]==1){
                     aux[combs[i]][k]=0;
                     r++;
-                }
-            }
-        }
-    }
+                }                        
         
+    for(i=0;i<n;i++)
+        free(aux[i]);
+    free(aux);
+    
     return r; 
 }
 
-void imprime_vetor(int vetor[], int n, int cont) {
-    int i;
-    for (i = 0; i < n; i++)
-        printf("%d ", vetor[i]);
-    printf("cont = %d\n", cont);
-}
-
-void combinar(int n, int nC, int combs[], int prox, int tam, int parl[], 
-              int salario[], int *cont, int k, int **infl){
-    int i, c=0, s=0, r, j;
+/* esse metodo faz a combinacao de nParls elementos dos n parlamentares, 
+ * verifica se a combinacao possui o numero minimo de parlamentares e soma os 
+ * salarios dos parlamentares contratados */
+void combinar(int n, int nParls, int *comb, int prox, int tam, int *somaInlf, 
+              int *salario, int k, int **infl, int *menor){    
+    /* p = parlamentares da combinacao; s = soma dos salarios; 
+     * r = parlamentares repetidos */
+    int i, p=0, s=0, r;
     
-    if(tam == nC){       
+    if(tam == nParls){       
         
-        for(i=0; i<nC; i++){
-            c+=parl[combs[i]];
-            s+=salario[combs[i]];
+        /* aqui as influencias e os salarios sao somados */
+        for(i=0; i<nParls; i++){
+            p+=somaInlf[comb[i]];
+            s+=salario[comb[i]];
         }
-        r = real(n, nC, combs, infl);
-        if(c-r>=k){                      
-            if(s<menor)
-                menor=s;
+        
+        /* se o salario atual for menor do que o menor geral, sao retirados os
+         * repetidos para checar se a combinacao possui o minimo de 
+         * parlamentares necessarios. uma pre-selecao eh realizada com p>=k
+         * para se evitar a utilizacao do metodo retiraRepetidos, que demanda
+         * muito processamento */
+        if(s<*menor && p>=k){                      
+            r = retiraRepetidos(n, nParls, comb, infl);
+            if(p-r>=k)
+                *menor=s;
         }
     }
+    
     else
-        for(i=prox; i<n; i++){
-            combs[tam] = i;
-            combinar(n, nC, combs, i + 1, tam + 1, parl, salario, cont, k, 
-                     infl);
+        for(i=prox;i<n;i++){
+            comb[tam] = i;
+            combinar(n, nParls, comb, i + 1, tam + 1, somaInlf, salario, k, 
+                     infl, menor);
         }
 }
 
 int main(){
-    /* n = numero de parlamentares; k = tamanho minimo do partido */
-    int n, k, i, j, l;
+    /* n = numero de parlamentares; k = tamanho minimo do partido */;
+    int n, k, i, j;
     /* salario = vetor dos salarios; infl = matriz influencia */
     int *salario, **infl;
-    
-    int parl[100], combs[100], cont = 0, nC=1;
+    /* somaInlf = soma das influencias de cada candidato; comb = combinacao 
+     atual; nParls = numero de parlamentares da combinacao atual;
+     menor = menor combinacao de salarios */
+    int *somaInlf, *comb, nParls=1, menor=0;
     
     /* entrada da primeira linha */
     scanf("%d %d", &n, &k);
@@ -81,39 +95,46 @@ int main(){
     for(i=0;i<n;i++)
         infl[i] = malloc(n * sizeof(int));
     
+    somaInlf = malloc(n * sizeof(int));
     for(i=0;i<n;i++)
-        parl[i]=0;
+        somaInlf[i]=0;
+    
+    comb = malloc(n * sizeof(int));
 
-    /* entrada dos salarios e das influencias */
-    for(i=0;i<n;i++)
+    /* entrada dos salarios e das influencias; o primero valor de menor sera
+     a soma de todos os salarios */
+    for(i=0;i<n;i++){
         scanf("%d", &salario[i]);
+        menor+= salario[i];
+    }
     
     for(i=0;i<n;i++)
         for(j=0;j<n;j++)
             scanf("%d", &infl[i][j]);
-                
+
+    /* contabilizacao da soma das influencias */
     for(i=0;i<n;i++){
         for(j=0;j<n;j++){
-            parl[i] +=  infl[i][j];
+            somaInlf[i] +=  infl[i][j];
         }
-    }
-        
+    }            
     
-
-    
-    while(nC<=n){
-        combinar(n, nC, combs, 0, 0, parl, salario, &cont, k, infl);
-        nC++;
+    /* o metodo sera executado fazendo combinacoes de 1 ate n elementos */
+    while(nParls<=n){
+        combinar(n, nParls, comb, 0, 0, somaInlf, salario, k, infl, &menor);
+        nParls++;
     }
-        
-        printf("%d\n", menor);
+    
+    /* impressao do menor valor encontrado */
+    printf("%d\n", menor);
         
     /* desalocacao */
-    free(salario);
-    
     for(i=0;i<n;i++)
         free(infl[i]);
     free(infl);
+    free(salario);
+    free(somaInlf);
+    free(comb);
     
     return 0;
 }
